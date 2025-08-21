35598ba3c5962417f783cbbccb4fb64e741c64a3
pedromst2000 <40210465@esmad.ipp.pt>
2025-08-21 22:15:30 +0100

    feat: Enhance Main class with sound handling and game state management

--- classes/Main.py
+++ classes/Main.py
@@ -1,21 +1,77 @@
+import pygame as game
+from utils.settings import CELL_NUMBER_X, CELL_NUMBER_Y
 from classes.Snake.Snake import Snake
+from classes.Byte.Byte import Byte
+
 
 class Main:
-    def __init__(self):
+    def __init__(
+        self,
+        eat_sound: game.mixer.Sound,
+        game_over_sound: game.mixer.Sound,
+        move_sound: game.mixer.Sound,
+    ):
         """
         Initialize the main game class.
         """
-        self.snake = Snake()   
+        self.snake = Snake()
+        self.byte = Byte(self.snake.body)
+        self.eat_sound = eat_sound
+        self.game_over_sound = game_over_sound
+        self.move_sound = move_sound
 
-
-    def update_snake(self):
+    def update_game(self):
         """
         Update the snake's position and state.
         """
         self.snake.move()
-    
-    def draw_snake(self, screen):
+        self.check_collision_byte(self.eat_sound)
+        self.check_fail()
+
+    def draw_elements(self, screen: game.Surface):
         """
-        Draw the snake on the given screen.
+        Draw the snake and byte on the given screen.
         """
-        self.snake.draw(screen)
+        self.byte.draw_byte(screen)
+        self.snake.draw_snake(screen)
+
+    def check_collision_byte(self, eat_sound: game.mixer.Sound):
+        """
+        Check for collisions with the byte.
+        """
+
+        if self.snake.body[0] == self.byte.pos:
+            eat_sound.play()
+            self.snake.add_block()
+            self.byte.randomize_position(self.snake.body)
+
+    def check_fail(self):
+        """
+        Check for collisions with the walls or self.
+        """
+
+        head = self.snake.body[0]
+
+        # Border collision (allowing last cell as valid)
+        if (
+            head.x < 0
+            or head.x >= CELL_NUMBER_X
+            or head.y < 0
+            or head.y >= CELL_NUMBER_Y
+        ):
+            self.game_over()
+            return
+
+        # Self collision
+        if head in self.snake.body[1:]:
+            self.game_over()
+
+    def game_over(self):
+        """
+        Handle game over state.
+        """
+        self.game_over_sound.play()
+        self.snake.reset()
+        self.byte.randomize_position(
+            self.snake.body
+        )  # Ensure byte is visible and not overlapping after reset
