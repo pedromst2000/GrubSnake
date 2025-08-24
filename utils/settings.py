639cf779844b359eff51786c4db30d4350807134
pedromst2000 <40210465@esmad.ipp.pt>
2025-08-24 15:48:08 +0100

    refactor: Reorganize game settings for clarity and update cell size and dimensions

--- utils/settings.py
+++ utils/settings.py
@@ -1,10 +1,16 @@
 import pygame as game
 
-CELL_SIZE = 40
-CELL_NUMBER_X = 25
-CELL_NUMBER_Y = 15
-SCREEN_WIDTH = CELL_SIZE * CELL_NUMBER_X  # 950
-SCREEN_HEIGHT = CELL_SIZE * CELL_NUMBER_Y  # 570
-FPS = 9.5
-MOVE_EVENT = game.USEREVENT + 1 # Event triggered for snake movement
-MOVE_INTERVAL_MS = 120 # milliseconds
+SCREEN_WIDTH = 950
+SCREEN_HEIGHT = 600
+
+# Snake & food size
+CELL_SIZE = 35 # the size of each cell in pixels 
+
+# Auto-calculate grid
+CELL_NUMBER_X = SCREEN_WIDTH // CELL_SIZE # number of cells in the x direction
+CELL_NUMBER_Y = SCREEN_HEIGHT // CELL_SIZE # number of cells in the y direction
+
+# Game speed
+FPS = 15
+MOVE_EVENT = game.USEREVENT + 1
+MOVE_INTERVAL_MS = 120
