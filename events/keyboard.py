2aa72b1c26b35c2c70555ee8b914e1a7ce343d70
pedromst2000 <40210465@esmad.ipp.pt>
2025-09-15 19:36:47 +0100

    refactor: Updated navigation event functions parameters

--- events/keyboard.py
+++ events/keyboard.py
@@ -9,7 +9,9 @@ def handle_keydown_navigation(
     selected_idx: int,
     num_buttons: int,
     buttons: list[Button],
-    screens: list[callable],
+    screens: list[callable] = None,
+    SCREEN: game.Surface = None,
+    selected_nvl: str = None,
 ) -> int:
     """
     Handles keydown events for menu navigation (up/down arrows and enter/space key).
@@ -19,11 +21,14 @@ def handle_keydown_navigation(
         selected_idx (int): The currently selected button index.
         num_buttons (int): The total number of buttons.
         buttons (list[Button]): The list of Button objects.
-        screens (list[callable]): A list of screen functions corresponding to the buttons.
+        screens (list[callable] | None): A list of screen functions corresponding to the buttons.
+        SCREEN (game.Surface | None): The main display surface where the screens will be rendered.
+        selected_nvl (str | None): The selected level (e.g., "easy", "medium", "hard").
 
     Returns:
         int: The updated selected button index.
     """
+
     if event.key == game.K_DOWN:
         selected_idx = (
             selected_idx + 1
@@ -42,12 +47,22 @@ def handle_keydown_navigation(
         selected_btn = buttons[selected_idx]  # Get the currently selected button
         if selected_btn.check_for_input(selected=True):  # Confirm selection
             SOUNDS["menu"]["click"].play()  # Click sound
-            if selected_btn.text_str == "PLAY":
-                screens[0]()  # Calling the function directly from the list of screens
-            elif selected_btn.text_str == "INSTRUCTIONS":
-                screens[1]()
-            elif selected_btn.text_str == "EXIT":
-                SOUNDS["menu"]["music"].stop()
-                game.quit()
-                sys.exit()
+
+            if screens and SCREEN is not None:
+                if selected_btn.text_str == "PLAY":
+                    screens[0](
+                        SCREEN
+                    )  # Calling the function directly from the list of screens
+                if selected_btn.text_str == "INSTRUCTIONS":
+                    screens[1](SCREEN)
+
+                elif selected_nvl is not None:
+                    if selected_btn.text_str in ["EASY", "MEDIUM", "HARD"]:
+                        # screens[0](SCREEN, selected_btn.text_str.lower())
+                        print(f"Selected level: {selected_nvl}")  # For testing purposes
+                if selected_btn.text_str == "EXIT":
+                    SOUNDS["menu"]["music"].stop()
+                    game.quit()
+                    sys.exit()
+
     return selected_idx
--- events/mouse.py
+++ events/mouse.py
@@ -8,9 +8,11 @@ def handle_mouse_navigation(
     event: game.event.Event,
     selected_idx: int,
     buttons: list[Button],
-    screens: list[callable],
+    screens: list[callable] | None = None,
     mouse_pos: tuple[int, int] = None,
-) -> None:
+    SCREEN: game.Surface = None,
+    selected_nvl: str = None,
+) -> int:
     """
     Handles mouse click events for menu navigation.
 
@@ -18,21 +20,34 @@ def handle_mouse_navigation(
         event (game.event.Event): The mouse button down event.
         selected_idx (int): The currently selected button index.
         buttons (list[Button]): The list of Button objects.
-        screens (list[callable]): A list of screen functions corresponding to the buttons.
+        screens (list[callable] | None, optional): A list of screen functions corresponding to the buttons. Defaults to None.
         mouse_pos (tuple[int, int], optional): The current mouse position. Defaults to None.
-
+        SCREEN (game.Surface, optional): The main display surface where the screens will be rendered. Defaults to None.
+        selected_nvl (str, optional): The selected level (e.g., "easy", "medium", "hard"). Defaults to None.
     Returns:
-        None
+        int: The updated selected button index.
     """
     SOUNDS["menu"]["click"].play()  # Click sound
-    for idx, button in enumerate(buttons):
-        if button.check_for_input(position=mouse_pos, selected=False):
+    for idx, button in enumerate(
+        buttons
+    ):  # Iterate through buttons to check each button it´s being clicked or selected
+        if button.check_for_input(position=mouse_pos):
             selected_idx = idx
-            if button.text_str == "PLAY":
-                screens[0]()  # Calling the function directly from the list of screens
-            elif button.text_str == "INSTRUCTIONS":
-                screens[1]()
-            elif button.text_str == "EXIT":
+
+            if screens and SCREEN is not None:  # Only proceed if screens are provided
+                if button.text_str == "PLAY":
+                    screens[0](
+                        SCREEN
+                    )  # Calling the function directly from the list of screens
+                if button.text_str == "INSTRUCTIONS":
+                    screens[1](SCREEN)
+                elif selected_nvl is not None:
+                    if button.text_str in ["EASY", "MEDIUM", "HARD"]:
+                        # screens[0](SCREEN, button.text_str.lower())
+                        print(f"Selected level: {selected_nvl}")
+            if button.text_str == "EXIT":
                 SOUNDS["menu"]["music"].stop()
                 game.quit()
                 sys.exit()
+
+    return selected_idx
