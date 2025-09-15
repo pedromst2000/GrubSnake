07523fe62bebd7038743fcbb90f94086eb1cce43
pedromst2000 <40210465@esmad.ipp.pt>
2025-09-15 19:40:42 +0100

    refactor: Update instructions_screen function to accept SCREEN parameter and improve rendering logic

--- screens/Instructions.py
+++ screens/Instructions.py
@@ -1,12 +1,25 @@
-def instructions_screen() -> None:
+import pygame as game
+
+
+def instructions_screen(SCREEN: game.Surface) -> None:
     """
     Displays the instructions for the game.
 
     Args:
-    - None
+    - SCREEN (game.Surface): The main display surface where the instructions will be rendered.
 
     Returns:
     - None
     """
 
-    print("INSTRUCTIONS SCREEN")
+    while True:
+
+        SCREEN.fill((0, 0, 0))  # Clear the screen with a black background
+
+        for event in game.event.get():
+            if event.type == game.QUIT:
+                game.quit()
+                exit()
+            print("INSTRUCTIONS SCREEN")
+                
+        game.display.update() 
